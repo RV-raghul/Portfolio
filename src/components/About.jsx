3113e8daa1519f813facb24465accd0f5c306887
RV-raghul <ravipanneer2001@gmail.com>
2025-08-28 00:36:55 +0530

    Changes done in About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,13 +15,10 @@ function About() {
         <div className="flex-1 text-center md:text-left">
           <h2 className="text-3xl md:text-4xl text-purple-400 font-bold mb-2">About Me</h2>
           <p className="text-white text-base md:text-lg mb-4">
-            I’m a Software Engineer, where I’ve been primarily focused on backend development, writing optimized SQL Server stored procedures and designing RDLC reports tailored to client needs.
+            I am a Software Engineer with a strong foundation in backend development, specializing in SQL Server and RDLC reporting. Expanding my expertise into Full Stack Web Development, I now work extensively with the MERN stack (MongoDB, Express.js, React.js, Node.js).
           </p>
           <p className="text-white text-base md:text-lg mb-4">
-            Driven by a strong desire to grow beyond traditional backend systems, I’ve upskilled into Full Stack Web Development. I now specialize in the MERN stack (MongoDB, Express.js, ReactJS, Node.js).
-          </p>
-          <p className="text-white text-base md:text-lg mb-4">
-            I’m passionate about crafting user-friendly, innovative, and scalable web applications that deliver seamless digital experiences. I enjoy turning ideas into fully functional, performant solutions using clean, maintainable code.
+            Passionate about creating scalable and user-friendly web applications, I focus on writing clean, maintainable code and delivering seamless digital experiences.
           </p>
         </div>
         {/* Image/GIF Section */}
